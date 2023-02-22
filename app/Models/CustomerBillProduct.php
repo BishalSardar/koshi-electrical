4e16ec142e075e58f5d14c74f3cd3006af7ad37c
Bishal Sardar <BishalSardar>
2023-02-22 19:28:58 +0545

    database manage

--- app/Models/CustomerBillProduct.php
+++ app/Models/CustomerBillProduct.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CustomerBillProducts extends Model
+class CustomerBillProduct extends Model
 {
     use HasFactory;
     protected $table = 'customer_bill_products';
